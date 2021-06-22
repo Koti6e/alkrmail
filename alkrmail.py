4617db188ab957bd8303017a1839f1de0d1d0292
Koti6e <86327670+Koti6e@users.noreply.github.com>
2021-06-23 03:23:50 +0530

    Update alkrmail.py

--- alkrmail.py
+++ alkrmail.py
@@ -5,7 +5,6 @@ import smtplib
 from os import system
 
 def main():
-   print """
   ____                 _ _ _   _            _
  / __|_ __ ___   __ _(_) | | | | __ _  ___| | __
 | ( | |  _   _ \ / _` | | | |_| |/ _` |/ __| |/ /
